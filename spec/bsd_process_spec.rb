af8e2a395474b4e9ddd5f872c23d20da79741e70
0x1eef <0x1eef@hardenedbsd.org>
2026-06-02 10:30:20 -0300

    specs: add meaningful tests

--- spec/bsd_process_spec.rb
+++
@@ -1,8 +0,0 @@
-describe "BSD" do
-  describe "constants" do
-    it "defines the module" do
-    end
-  end
-end
-
-Minitest.run(ARGV) || exit(1)
---
+++ spec/process_spec.rb
@@ -0,0 +1,23 @@
+describe "Kernel::Process" do
+  describe ".self" do
+    subject { Kernel::Process.self }
+
+    it "returns an instance of Kernel::Process" do
+      expect(subject.class).must_equal  Kernel::Process
+    end
+
+    it "returns the pid" do
+      expect(subject.pid).must_equal Process.pid
+    end
+
+    it "returns the parent pid" do
+      expect(subject.ppid).must_equal Process.ppid
+    end
+
+    it "returns the uid" do
+      expect(subject.uid).must_equal Process.uid
+    end
+  end
+end
+
+Minitest.run(ARGV) || exit(1)
